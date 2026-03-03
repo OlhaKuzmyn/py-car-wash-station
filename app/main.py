26476d65e208d0c4ff067cc382c573066446f1a2
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-03-03 23:33:52 +0200

    changed income variable, added condition in wash_single_car

--- app/main.py
+++ app/main.py
@@ -22,15 +22,15 @@ class CarWashStation:
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
-        self.income = 0
 
     def serve_cars(self, list_cars: list[Car]) -> float:
+        income = 0
         for car in list_cars:
             if car.clean_mark < self.clean_power:
-                self.income += self.calculate_washing_price(car)
+                income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
-        self.income = round(self.income, 1)
-        return self.income
+        income = round(income, 1)
+        return income
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
@@ -42,7 +42,8 @@ class CarWashStation:
         )
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rating: int | float) -> None:
         self.average_rating = round(
